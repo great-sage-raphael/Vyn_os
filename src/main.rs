191d923bb31d9b44bafe56835ef1b55f7d0e90a1
vinayak prakash <vinayakprakash2004@gmail.com>
2025-04-27 23:28:46 +0530

    bare-bones_done

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+#![no_std]// don't link the Rust standard library
+#![no_main]  // disable all Rust-level entry points
+
+mod vga_buffer;
+use core::panic::PanicInfo;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    // This function is the entry point, since the linker looks for a function    // named `_start` by default
+    println!("Hello World{}", "!");
+
+    loop {}
+    
+}
